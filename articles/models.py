ca84434979590df040d24057b2e356942805b564
Alireza Sadeghi <aliraza7926@gmail.com>
2021-02-18 15:32:21 +0330

    fix article field name from auth to author

--- articles/models.py
+++ articles/models.py
@@ -7,7 +7,7 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    auth = models.ForeignKey(
+    author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
     )
