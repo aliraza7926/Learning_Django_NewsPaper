d9baf3438a3342b074d54ec5e26cf4648869d7d6
Alireza Sadeghi <aliraza7926@gmail.com>
2021-02-18 11:46:48 +0330

    add crispy to installed app

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+    'crispy_froms', 
+
+    # Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
